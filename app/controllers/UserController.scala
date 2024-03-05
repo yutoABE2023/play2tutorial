2197c7761dd2d4b358ae3777b15c4d621185bf33
Yuto ABE <y.abe@jus.co.jp>
2024-03-05 11:05:55 +0900

    ユーザ登録・編集画面の実装

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -30,7 +30,33 @@ class UserController @Inject()(components: MessagesControllerComponents)
   /**
    * 編集画面表示
    */
-  def edit(id: Option[Long]) = TODO
+  // コンパニオンオブジェクトに定義したFormを参照するためにimport文を追加
+
+  import UserController._
+
+  private val c = Companies.syntax("c")
+
+  def edit(id: Option[Long]) = Action { implicit request =>
+    DB.readOnly { implicit session =>
+      // リクエストパラメータにIDが存在する場合
+      val form = id match {
+        // IDが渡されなかった場合は新規登録フォーム
+        case None => userForm
+        // IDからユーザ情報を1件取得してフォームに詰める
+        case Some(id) => Users.find(id) match {
+          case Some(user) => userForm.fill(UserForm(Some(user.id), user.name, user.companyId))
+          case None => userForm
+        }
+      }
+
+      // プルダウンに表示する会社のリストを取得
+      val companies = withSQL {
+        select.from(Companies as c).orderBy(c.id.asc)
+      }.map(Companies(c.resultName)).list().apply()
+
+      Ok(views.html.user.edit(form, companies))
+    }
+  }
 
   /**
    * 登録実行
@@ -47,4 +73,18 @@ class UserController @Inject()(components: MessagesControllerComponents)
    */
   def remove(id: Long) = TODO
 
+}
+
+object UserController {
+  // フォームの値を格納するケースクラス
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  // formから送信されたデータ ⇔ ケースクラスの変換を行う
+  val userForm = Form(
+    mapping(
+      "id"        -> optional(longNumber),
+      "name"      -> nonEmptyText(maxLength = 20),
+      "companyId" -> optional(number)
+    )(UserForm.apply)(UserForm.unapply)
+  )
 }
